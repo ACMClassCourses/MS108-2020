bc1e71f4b6c84823c628ccf0add7ed9756fb5843
ZYHowell <1148830469@qq.com>
2020-11-30 22:49:14 +0800

    FPGA Test

--- riscv/src/hci.v
+++ riscv/src/hci.v
@@ -250,6 +250,7 @@ always @*
               d_tx_data = 8'h00;
               d_wr_en = 1'b1;
             end
+            d_state = S_DECODE; 
             $display("IO:Return");
             $finish;
           end
